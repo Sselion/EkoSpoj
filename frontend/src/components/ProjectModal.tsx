b4646125821f51d608512887f7390c70eee870e1
Martina Polášková <104367983+polaskovam@users.noreply.github.com>
2023-12-18 22:06:23 +0100

    init frontend

---
+++ frontend/src/components/ProjectModal.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ProjectModal() {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default ProjectModal;
---
+++ frontend/src/components/SubHeader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SubHeader() {
+    return(
+        <div>
+        {/*texty o nás, + popisy kategorií*/}
+        </div>
+    )
+}
+
+export default SubHeader;
