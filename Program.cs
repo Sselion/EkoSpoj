fb78224a60fc139955d14cc4087e8176b10c1fc9
Ludek_Naiman <ludek.naiman@tul.cz>
2024-01-09 22:20:08 +0100

    init

--- Program.cs
+++ Program.cs
@@ -1,17 +1,28 @@
 using Ekospoj.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddRazorPages();
 // Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer(); ;
 
-builder.Services.AddDbContext<ProjectDbContext>( options =>
+builder.Services.AddDbContext<ProjectDbContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("ProjectConnectionString"))
     );
 var app = builder.Build();
+
+if (!builder.Environment.IsDevelopment())
+{
+    builder.Services.AddHttpsRedirection(options =>
+    {
+        options.RedirectStatusCode = (int)HttpStatusCode.PermanentRedirect;
+        options.HttpsPort = 443;
+    });
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
