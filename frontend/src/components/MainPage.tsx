e195ed54db3dcb70160aa20be9b888831e1802b5
Martina Polášková <104367983+polaskovam@users.noreply.github.com>
2024-01-10 22:26:43 +0100

    mockup fetch, minor fixes

--- frontend/src/components/MainPage.tsx
+++ frontend/src/components/MainPage.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import Box from "@mui/material/Box";
+import React, {useEffect, useState} from "react";
 import NavBar from "./NavBar";
 import Heading from "./Heading";
-import CategorySelection from "./CategorySelection";
+import CategoryNavigation from "./CategoryNavigation";
 
 
 const name = "O nás";
@@ -11,11 +10,33 @@ const about = "Tady naleznete ekologické projekty, které jsou aktivní v ČR.T
     "                ekologické projekty, které jsou aktivní v ČR. Tady naleznete ekologické projekty, které jsou aktivní v\n" +
     "                ČR. Tady naleznete ekologické projekty, které jsou aktivní v ČR.";
 function MainPage() {
+    const [data, setData] = useState<Record<string, any>[]>([]);
+
+    useEffect(() => {
+        fetch("https://ekospoj.cz/api/Categories", {
+            method: 'GET',
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                return response.json();
+            })
+            .then(json => {
+                console.log(json);
+                setData(json);
+            })
+            .catch(error => {
+                window.alert(error);
+            });
+    }, []);
+
     return(
         <>
             <NavBar/>
             <Heading name={name} about={about} size={10} />
-            <CategorySelection />
+            <CategoryNavigation apiCategories={data} />
         </>
 
     )
