4e15c8f4dab5c7a9c5dcaafed441fa837404c784
Martina Polášková <104367983+polaskovam@users.noreply.github.com>
2024-01-26 21:57:35 +0100

    add login, structure for admin

---
+++ frontend/src/components/admin/AdminPage.tsx
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+
+
+
+// TODO: loginform; login check; form for new project, send it to BE; logout
+function AdminPage() {
+    return(
+        <Box>
+
+        </Box>
+    )
+}
+
+export default AdminPage;
---
+++ frontend/src/components/admin/LoginForm.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+
+
+interface FormData {
+    name: string;
+    password: string;
+}
+
+function LoginForm() {
+    const [formData, setFormData] = useState<FormData>({
+        name: "",
+        password: ""
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        const { name, value } = e.target;
+
+        setFormData((prevData: FormData) => ({
+            ...prevData, [name]: value
+        }))
+    };
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log(formData)
+        setFormData({
+            name: "",
+            password: ""
+        });
+    }
+
+    return (
+        <Box sx={{ p: 3, textAlign: "center", maxWidth: "400px", margin: '0 auto', mt: 15 }}>
+            <Stack spacing={1}>
+                <TextField
+                    id="login-name"
+                    label="Login"
+                    name="name"
+                    sx={{ width: '100%' }}
+                    onChange={handleChange}
+                    value={formData.name}
+                />
+                <TextField
+                    id="login-password"
+                    label="Password"
+                    name="password"
+                    type="password"
+                    sx={{ width: '100%' }}
+                    onChange={handleChange}
+                    value={formData.password}
+                />
+            </Stack>
+            <Button type="submit" onClick={handleSubmit}
+                    sx={{
+                        mt: 2,
+                        backgroundColor: "#83C089",
+                        color: "white"
+                    }}
+            >
+                <Typography variant="subtitle1" display="block">
+                    Přihlásit se
+                </Typography>
+            </Button>
+        </Box>
+    )
+}
+
+export default LoginForm;
